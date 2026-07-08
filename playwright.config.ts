9bbec5d797e30f55d6c39312db39b61dfc8cbb56
Chakra <ballachakri@yahoo.com>
2026-07-08 16:09:03 +0100

    reinstalled npm

--- playwright.config.ts
+++ playwright.config.ts
@@ -44,7 +44,7 @@ export default defineConfig({
   /* Configure projects for major browsers */
   projects: [
     {
-      name: 'chromium',
+      name: 'Playwright_TypeScript',
       use: { ...devices['Desktop Chrome'] },
     },
 
